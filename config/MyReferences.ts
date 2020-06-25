a1751d3e349f15908105d6ad9a1b7dc28b1dde6f
nwesthoff <nilsepils94@hotmail.com>
2020-06-25 19:32:22 +0200

    📝 Writing about spotify culture

--- config/MyReferences.ts
+++ config/MyReferences.ts
@@ -9,6 +9,16 @@ const myReferences: AcademicRef[] = [
         "Singer, R. and Basecamp (2019). Shape Up: Stop Running in Circles and Ship Work that Matters. [online] Shape Up. Available at: https://basecamp.com/shapeup/webbook [Accessed 25 Jun. 2020].",
     },
   },
+  {
+    citationKey: "spotifyCulture",
+    entryType: "TRUSTED",
+    entryTags: {
+      author: "Kniberg and Spotify",
+      year: "2014",
+      inBib:
+        "Kniberg, H. and Spotify (2014). Spotify Engineering Culture. [online] Spotify Engineering. Available at: https://engineering.atspotify.com/2014/03/27/spotify-engineering-culture-part-1/ [Accessed 25 Jun. 2020].",
+    },
+  },
 ];
 
 export interface AcademicRef {
